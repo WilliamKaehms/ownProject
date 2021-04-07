a4c3f633418e8c57e2e20f58908d35b648a47194
WilliamKaehms <william.kaehms@gmx.de>
2021-04-07 11:12:15 +0200

    Hallo Welt program with different languages

--- src/hallowelt.cc
+++ src/hallowelt.cc
@@ -1,6 +1,10 @@
 #include<iostream>
+#include<string>
 
 int main(){
-	std::cout<< "Hallo Welt" << std::endl;
+	enum languages {german, english, french};
+	std::string greetings[3] = {"Hallo Welt!","Hello world!","Salut monde!"};
+	languages lang = french;
+	std::cout << greetings[lang] << std::endl;
 	return 0;
 }
